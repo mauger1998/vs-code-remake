f11b962042c370f2cf7857ebc684916f70cdc660
mauger1998 <110930037+mauger1998@users.noreply.github.com>
2022-11-03 21:18:23 +0000

    Add apple svg and script to draw as scroll

--- index.js
+++ index.js
@@ -94,3 +94,20 @@ const workButtonScroll = document.getElementById("scroll-to-work__button")
 workButtonScroll.addEventListener("click", () => {
     workButton_scrollTo.scrollIntoView()
 })
+
+// SVG
+
+let path = document.querySelector("path")
+let pathLength = path.getTotalLength()
+
+path.style.strokeDasharray = pathLength + " " + pathLength
+
+path.style.strokeDashoffset = pathLength
+
+window.addEventListener("scroll", () => {
+    var scrollPercentage = (document.documentElement.scrollTop + document.body.scrollTop) / (document.documentElement.scrollHeight - document.documentElement.clientHeight)
+
+    var drawLength = pathLength * scrollPercentage;
+
+    path.style.strokeDashoffset = pathLength - drawLength
+})
